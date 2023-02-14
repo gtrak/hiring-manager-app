001d8056a12bf1de6e515f7f262df4f5a8b55a8d
Gary Trakhman <gary.trakhman@gmail.com>
2023-02-14 17:08:44 -0500

    Display mock data

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Nav from "./Nav";
+import User, { responseInfo, mockResult } from "./User";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <div>
       <Nav />
+      <User {...responseInfo(mockResult)} />
     </div>
   </React.StrictMode>
 );
