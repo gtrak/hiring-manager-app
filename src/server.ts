68d0bfef5ec2b4977e877b9e57212cfca0c45e7b
Gary Trakhman <gary.trakhman@gmail.com>
2023-02-14 15:42:05 -0500

    Fixed API body handling

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import Koa from "koa";
 import KoaRouter from "koa-router";
-import Sqlite3 from "sqlite3";
+import { koaBody } from "koa-body";
 
+import Sqlite3 from "sqlite3";
 const logger = require("pino")();
 
 const port = Number(process.env["SERVER_PORT"] || 3000);
@@ -13,10 +14,15 @@ logger.info({ msg: "Starting app", port });
 const dbPath = process.env["SERVER_DB"] || "local/candidates.sqlite";
 logger.info(`Initializing database in ${dbPath}`);
 
-const db = new (Sqlite3.verbose().Database)(dbPath);
+const openDb = () => {
+  return new (Sqlite3.verbose().Database)(dbPath);
+};
 
-/* This is just a low-tech migration script example, I would use a real library in production */
-const migrations = `
+(() => {
+  const db = openDb();
+
+  /* This is just a low-tech migration script example, I would use a real library in production */
+  const migrations = `
 CREATE TABLE IF NOT EXISTS candidates (
     id uuid primary key not null,
     first_name text,
@@ -25,34 +31,52 @@ CREATE TABLE IF NOT EXISTS candidates (
     note text
 )
 `;
+  logger.info({ msg: "Running migrations" });
+  db.all(migrations);
+  logger.info({ msg: "Running migrations finished" });
 
-logger.info({ msg: "Running migrations" });
-db.all(migrations);
-logger.info({ msg: "Running migrations finished" });
-
-db.close();
+  db.close();
+})();
 
 const app = new Koa();
 
-const router = new KoaRouter();
+interface WithDb extends Koa.Context {
+  db: Sqlite3.Database;
+}
 
-router.get("/status", async (ctx) => {
-  ctx.status = 200;
-  ctx.body = "OK";
+const router = new KoaRouter<Koa.DefaultState, WithDb>();
+
+router.post("/candidate", async (ctx) => {
+  const db = ctx.db;
+  const { first_name, last_name, email, note } = ctx.request.body;
+  db;
 });
 
 const logRequest: Koa.Middleware = async (ctx, next) => {
   logger.info({ msg: "Request", request: ctx.request });
-  await next();
+  return await next();
+};
+
+const db: Koa.Middleware = async (ctx, next) => {
+  const db = openDb();
+  ctx.db = db;
+  try {
+    return await next();
+  } finally {
+    db.close();
+    ctx.db = undefined;
+  }
 };
 
 const logResponse: Koa.Middleware = async (ctx, next) => {
   logger.info({ msg: "Response", response: ctx.response });
-  await next();
+  return await next();
 };
 
 app
   .use(logRequest)
+  .use(koaBody())
+  .use(db)
   .use(router.routes())
   .use(router.allowedMethods())
   .use(logResponse)
