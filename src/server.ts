6d62dc437337aafddc61abc4954aa0bc77148a1b
Gary Trakhman <gary.trakhman@gmail.com>
2023-02-14 14:14:34 -0500

    DB 'migrations' work

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,36 @@
 import Koa from "koa";
 import KoaRouter from "koa-router";
+import Sqlite3 from "sqlite3";
 
 const logger = require("pino")();
 
 const port = Number(process.env["SERVER_PORT"] || 3000);
-
 if (isNaN(port)) {
   throw new Error(`invalid SERVER_PORT: ${process.env["SERVER_PORT"]}`);
 }
 
 logger.info({ msg: "Starting app", port });
+const dbPath = process.env["SERVER_DB"] || "local/candidates.sqlite";
+logger.info(`Initializing database in ${dbPath}`);
+
+const db = new (Sqlite3.verbose().Database)(dbPath);
+
+/* This is just a low-tech migration script example, I would use a real library in production */
+const migrations = `
+CREATE TABLE IF NOT EXISTS candidates (
+    id uuid primary key not null,
+    first_name text,
+    last_name text,
+    email text,
+    note text
+)
+`;
+
+logger.info({ msg: "Running migrations" });
+db.all(migrations);
+logger.info({ msg: "Running migrations finished" });
+
+db.close();
 
 const app = new Koa();
 
